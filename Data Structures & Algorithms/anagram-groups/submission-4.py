e9c704fc5d2f4ff715e98049d10a4cf4a5aeb55a
Sashikumar Nehrumohan <76261337+sashikumar6@users.noreply.github.com>
2026-08-14 14:06:03 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        freq=defaultdict(list)
+        
+
+        for word in strs:
+            count=[0]*26
+            for ch in word:
+                count[ord(ch)-ord('a')]+=1
+            freq[tuple(count)].append(word)
+        
+     
+        return list(freq.values())
+
+        
+        
